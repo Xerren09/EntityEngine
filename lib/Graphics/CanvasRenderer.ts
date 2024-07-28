bf1f1fc17d3cfcb532495c10d59300dea5ac1e16
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2024-07-28 18:38:29 +0300

    Update CanvasRenderer.ts

--- lib/Graphics/CanvasRenderer.ts
+++ lib/Graphics/CanvasRenderer.ts
@@ -1,17 +1,35 @@
+import { rectSize } from "../Types/Types";
+
+/**
+ * Provides access to the canvas API attached to an engine instance.
+ */
 export class CanvasRenderer {
     /**
      * The HTML Canvas instance the engine will render frames on.
      */
     public readonly canvas: HTMLCanvasElement;
     /**
-     * The rendering context for {@link canvas}.
+     * The 2D rendering context for {@link canvas}.
      */
     public readonly context: CanvasRenderingContext2D;
+    /**
+     * Gets the current size of the canvas in pixels.
+     */
+    public get size(): rectSize {
+        return {
+            width: this.canvas.width,
+            height: this.canvas.height
+        };
+    }
     /**
      * Contains a set of engine specific options for rendering.
      */
     public options: RendererOptions = {};
 
+    /**
+     * 
+     * @param htmlCanvasID The HTMLCanvasElement's ID in the document.
+     */
     constructor(htmlCanvasID: string) {
         const canvas = document.getElementById(htmlCanvasID) as HTMLCanvasElement;
         if (canvas == undefined) {
@@ -32,7 +50,9 @@ export class CanvasRenderer {
     }
 }
 
-
+/**
+ * Contains possible settings for the renderer.
+ */
 export interface RendererOptions {
     /**
      * Sets whether entities' colliders should be drawn on top of their sprites.
